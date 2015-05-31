8bed86f34e1746c4217bcc6fd2bda1dd45c7ef2d
CageStooge <caged.stooge@gmail.com>
2015-05-31 15:14:25 -0400

    Fixed Naming structure

--- Skylines-EnhancedBuildPanel/Properties/AssemblyInfo.cs
+++ Skylines-EnhancedBuildPanel/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Skylines-EnhancedBuildPanel")]
+[assembly: AssemblyTitle("EnhnacedBuildPanel")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Skylines-EnhancedBuildPanel")]
+[assembly: AssemblyProduct("EnhancedBuildPanel")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
